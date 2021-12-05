65bfb8be6f134ce44dced2120269274445e176bb
a1shuvo <a1shuvosaha@gmail.com>
2021-12-05 11:46:12 +0600

    Handling Exception FileNoTFoundError

--- main.py
+++ main.py
@@ -47,16 +47,21 @@ def save():
         is_ok = messagebox.askokcancel(title=website, message=f"the details you have entered:\nEmail: {email}\n"
                                                               f"Password: {new_pass}\nIs it Ok to Save?")
         if is_ok:
-            with open("data.json", "r") as data_file:
-                # Reading old data
-                data = json.load(data_file)
+            try:
+                with open("data.json", "r") as data_file:
+                    # Reading old data
+                    data = json.load(data_file)
+            except FileNotFoundError:
+                with open("data.json", "w") as data_file:
+                    # Saving updated data
+                    json.dump(new_data, data_file, indent=4)
+            else:
                 # Updating old data with new data
                 data.update(new_data)
-
-            with open("data.json", "w") as data_file:
-                # Saving updated data
-                json.dump(data, data_file, indent=4)
-
+                with open("data.json", "w") as data_file:
+                    # Saving updated data
+                    json.dump(data, data_file, indent=4)
+            finally:
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
 
